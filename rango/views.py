cda006a790d4282ece96d0acf64aadc3a4b18871
Ross Clark <2251999C@student.gla.ac.uk>
2018-02-12 19:05:55 +0000

    10.4 complete

--- rango/views.py
+++ rango/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 
 def index(request):
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
@@ -15,6 +16,9 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     return render(request, "rango/about.html")
 
 def show_category(request, category_name_slug):
